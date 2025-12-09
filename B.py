a46f5c6f8d4dd249be4c73b0aeb41d2a9da6d964
tushar_dev <73417554+tushar-dev061@users.noreply.github.com>
2025-12-09 22:10:39 +0530

    Update B.py

--- B.py
+++ B.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask('')
+app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return 'BOT IS SERVING YOU'
+@app.route("/")
+def index():
+    return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 10000))  # Render auto port
+    app.run(host="0.0.0.0", port=port)
 
 def b():
     server = Thread(target=run)
+    server.daemon = True  # ensures thread closes on shutdown
     server.start()
